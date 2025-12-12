5d8ea671dadaa04193443b32ef74a947f335409a
categoraal <76538493+categoraal@users.noreply.github.com>
2025-12-12 08:49:12 +0100

    day 12 codegolfed a bit

--- 12.py
+++ 12.py
@@ -1,18 +1,10 @@
-data = open('in12').read().strip().split('\n\n')
-presents = [i.split('\n')[1:] for i in data[:-1]]
-shapes = [[j for j in i.split(' ')] for i in data[-1].split('\n')]
+d = open('in12').read().split('\n\n')
+p = [i.count('#') for i in d[:-1]]
+sh = [[j for j in i.split(' ')] for i in d[-1].split('\n')]
 
 p1 = 0
-for line in shapes:
-    size = [int(i) for i in line[0][:-1].split('x')]
-    size = size[0]*size[1]
-    idxs = line[1:]
-    sp = 0
-    for idx,c in enumerate(idxs):
-        a = presents[idx]
-        sa = ''.join(a).count('#')
-        sp += int(c)*sa
-    diff = size-sp
-    if diff > 0:
-        p1 += 1
+for l in sh:
+    s = [int(i) for i in l[0][:-1].split('x')]
+    s = s[0]*s[1]
+    if s-sum([p[idx]*int(c) for idx,c in enumerate(l[1:])])>0:p1 += 1
 print(p1)
